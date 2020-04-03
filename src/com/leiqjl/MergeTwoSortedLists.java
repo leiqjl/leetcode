40e0e421abd2b230eb97e0497bf6a4a1a2f49c25
leiqjl <leiqjl@gmail.com>
2020-04-04 00:41:31 +0800

    @add MergeTwoSortedLists

---
+++ src/com/leiqjl/MergeTwoSortedLists.java
@@ -0,0 +1,33 @@
+package com.leiqjl;
+
+/**
+ * Merge two sorted linked lists and return it as a new list.
+ * The new list should be made by splicing together the nodes of the first two lists.
+ */
+public class MergeTwoSortedLists {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode root = new ListNode(0);
+        ListNode node = root;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                node.next = l1;
+                l1 = l1.next;
+            } else {
+                node.next = l2;
+                l2 = l2.next;
+            }
+            node = node.next;
+        }
+        node.next = l1 != null ? l1 : l2;
+        return root.next;
+    }
+}
