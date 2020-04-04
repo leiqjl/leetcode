d232ea44d3af1ba001f721050382a15e8fa1f191
leiqjl <leiqjl@gmail.com>
2020-04-05 01:43:47 +0800

    @add Implement strStr()

---
+++ src/com/leiqjl/ImplementStrStr.java
@@ -0,0 +1,29 @@
+package com.leiqjl;
+
+/**
+ * Implement strStr().
+ *
+ * Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ *
+ */
+public class ImplementStrStr {
+
+    public int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) {
+            return 0;
+        }
+
+        for (int i = 0; i <=haystack.length()-needle.length(); i++) {
+            for (int j = 0; j < needle.length(); j++) {
+                if (haystack.charAt(i+j) != needle.charAt(j)) {
+                    break;
+                }
+                if (j == needle.length()-1) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+}
