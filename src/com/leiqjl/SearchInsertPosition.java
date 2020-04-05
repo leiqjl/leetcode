0bebc29b1d664cfc00c9f882a1181c1f7e35abf2
leiqjl <leiqjl@gmail.com>
2020-04-05 15:07:27 +0800

    @add Search Insert Position

---
+++ src/com/leiqjl/SearchInsertPosition.java
@@ -0,0 +1,21 @@
+package com.leiqjl;
+
+/**
+ * Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+ *
+ * You may assume no duplicates in the array.
+ *
+ */
+public class SearchInsertPosition {
+
+    public int searchInsert(int[] nums, int target) {
+        int i;
+        for (i = 0; i < nums.length; i++) {
+            if (nums[i] >= target) {
+                break;
+            }
+        }
+        return i;
+    }
+
+}
