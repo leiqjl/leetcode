e255681acd70f4f2c9f04d28b5f308c96e5b73a9
leiqjl <leiqjl@gmail.com>
2020-04-14 01:08:03 +0800

    @add RemoveDuplicatesFromSortedList

---
+++ src/com/leiqjl/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,29 @@
+package com.leiqjl;
+
+public class RemoveDuplicatesFromSortedList {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode node = head;
+        while (node.next != null) {
+            if (node.val == node.next.val) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
+            }
+        }
+        return head;
+    }
+
+}
