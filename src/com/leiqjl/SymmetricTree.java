6841d0d38630cc0a24932db2649a0e889da659a4
leiqjl <leiqjl@gmail.com>
2020-04-21 00:19:33 +0800

    @add SymmetricTree

---
+++ src/com/leiqjl/SymmetricTree.java
@@ -0,0 +1,31 @@
+package com.leiqjl;
+
+/**
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+ */
+public class SymmetricTree {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return (root==null) || isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null || left.val != right.val) {
+            return false;
+        }
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+}
