0b09720d82c9e29c476cd43fa0741a8da56ff74e
leiqjl <leiqjl@gmail.com>
2020-04-22 00:18:33 +0800

    @add MaximumDepthOfBinaryTree

---
+++ src/com/leiqjl/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,30 @@
+package com.leiqjl;
+
+/**
+ * Given a binary tree, find its maximum depth.
+ *
+ * The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ *
+ * Note: A leaf is a node with no children.
+ *
+ */
+public class MaximumDepthOfBinaryTree {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+}
