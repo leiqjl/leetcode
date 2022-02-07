5f95f941c14e4cc3173da66d82ba5a590ac41402
leiqjl <leiqjl@gmail.com>
2022-02-07 16:32:42 +0800

    @add Palindrome Linked List

---
+++ src/com/leiqjl/PalindromeLinkedList.java
@@ -0,0 +1,47 @@
+package com.leiqjl;
+
+/**
+ * 234. Palindrome Linked List
+ *
+ * Given the head of a singly linked list, return true if it is a palindrome.
+ *
+ */
+public class PalindromeLinkedList {
+
+
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if (fast != null) {
+            slow = slow.next;
+        }
+        ListNode right = reverse(slow);
+        ListNode left = head;
+        while (right != null) {
+            if (left.val != right.val) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
+
+    public ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode pre = null, cur = head, next;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+}
