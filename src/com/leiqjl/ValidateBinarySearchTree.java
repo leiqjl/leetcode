576498675132a527161933fd86e072dfbe300cc4
leiqjl <leiqjl@gmail.com>
2022-02-17 00:15:59 +0800

    @add Validate Binary Search Tree

---
+++ src/com/leiqjl/ValidateBinarySearchTree.java
@@ -0,0 +1,33 @@
+package com.leiqjl;
+
+/**
+ * 98. Validate Binary Search Tree - Medium
+ *
+ * Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+ *
+ * A valid BST is defined as follows:
+ * - The left subtree of a node contains only nodes with keys less than the node's key.
+ * - The right subtree of a node contains only nodes with keys greater than the node's key.
+ * - Both the left and right subtrees must also be binary search trees.
+ *
+ */
+public class ValidateBinarySearchTree {
+
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, null, null);
+    }
+
+    private boolean isValidBST(TreeNode root, TreeNode min, TreeNode max) {
+        if (root == null) {
+            return true;
+        }
+        if (min != null && root.val <= min.val) {
+            return false;
+        }
+        if (max != null && root.val >= max.val) {
+            return false;
+        }
+        return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+    }
+
+}
