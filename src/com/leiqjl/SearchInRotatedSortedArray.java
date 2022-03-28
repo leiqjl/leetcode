f6ca725104937143536ce8b080609921d46fc727
leiqjl <leiqjl@gmail.com>
2022-03-28 15:09:02 +0800

    @update Search in Rotated Sorted Array

--- src/com/leiqjl/SearchInRotatedSortedArray.java
+++ src/com/leiqjl/SearchInRotatedSortedArray.java
@@ -44,6 +44,32 @@ public class SearchInRotatedSortedArray {
         return -1;
     }
 
+    public int search1(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int begin = 0, end = nums.length - 1;
+        while (begin <= end) {
+            int mid = (begin + end) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[begin] <= nums[mid]) {
+                if ((nums[begin] <= target) && (target < nums[mid])) {
+                    end = mid - 1;
+                } else {
+                    begin = mid + 1;
+                }
+            } else {
+                if ((nums[mid] < target) && (target <= nums[end])) {
+                    begin = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
 
 
     public static void main(String[] args) {
@@ -52,12 +78,15 @@ public class SearchInRotatedSortedArray {
         //Output: 4
         int[] nums = {4, 5, 6, 7, 0, 1, 2};
         assert s.search(nums, 0) == 4;
+        assert s.search1(nums, 0) == 4;
         //Input: nums = [4,5,6,7,0,1,2], target = 3
         //Output: -1
         int[] nums1 = {4, 5, 6, 7, 0, 1, 2};
         assert s.search(nums1, 3) == -1;
+        assert s.search1(nums1, 3) == -1;
         int[] nums2 = {1};
         assert s.search(nums2, 0) == -1;
+        assert s.search1(nums2, 0) == -1;
 
     }
 }
