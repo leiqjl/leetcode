3d8aabba4984aa4039729372d4e9cff96744e4cf
leiqjl <leiqjl@gmail.com>
2022-04-06 22:40:03 +0800

    @add Next Permutation

---
+++ src/com/leiqjl/NextPermutation.java
@@ -0,0 +1,39 @@
+package com.leiqjl;
+
+/**
+ * 31. Next Permutation - Medium
+ */
+public class NextPermutation {
+
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0) {
+            if (nums[i] < nums[i + 1]) {
+                break;
+            }
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int begin, int end) {
+        while (begin < end) {
+            swap(nums, begin, end);
+            begin++;
+            end--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
