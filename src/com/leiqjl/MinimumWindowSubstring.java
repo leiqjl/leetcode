ac9d30e38bb54a28dc82e89018540e57270ad71d
leiqjl <leiqjl@gmail.com>
2022-04-14 10:23:01 +0800

    @add Minimum Window Substring

---
+++ src/com/leiqjl/MinimumWindowSubstring.java
@@ -0,0 +1,65 @@
+package com.leiqjl;
+
+import java.util.HashMap;
+
+/**
+ * 76. Minimum Window Substring - Hard
+ * <p>
+ * Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every character in t (including duplicates) is included in the window.
+ * If there is no such substring, return the empty string "".
+ * <p>
+ * The testcases will be generated such that the answer is unique.
+ * <p>
+ * A substring is a contiguous sequence of characters within the string.
+ */
+public class MinimumWindowSubstring {
+
+    public String minWindow(String s, String t) {
+        int left = 0, right = 0, count = 0;
+        int min = Integer.MAX_VALUE, sub = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> tmap = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            tmap.put(c, tmap.getOrDefault(c, 0) + 1);
+        }
+        while (right < s.length()) {
+            char cr = s.charAt(right);
+            if (tmap.containsKey(cr)) {
+                int crCount = map.getOrDefault(cr, 0) + 1;
+                map.put(cr, crCount);
+                if (crCount == tmap.get(cr)) {
+                    count++;
+                }
+            }
+            while (count == tmap.size()) {
+                if ((right - left + 1) < min) {
+                    min = right - left + 1;
+                    sub = left;
+                }
+                char cl = s.charAt(left);
+                if (tmap.containsKey(cl)) {
+                    int clCount = map.get(cl);
+                    if (clCount == tmap.get(cl)) {
+                        count--;
+                    }
+                    map.put(cl, clCount - 1);
+                }
+                left++;
+            }
+            right++;
+        }
+        return min == Integer.MAX_VALUE ? "" : s.substring(sub, sub + min);
+    }
+
+    public static void main(String[] args) {
+        MinimumWindowSubstring m = new MinimumWindowSubstring();
+        //Input: s = "ADOBECODEBANC", t = "ABC"
+        //Output: "BANC"
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+        assert m.minWindow(s, t).equals("BANC");
+        String s1 = "a";
+        String t1 = "aa";
+        assert m.minWindow(s1, t1).equals("");
+    }
+}
