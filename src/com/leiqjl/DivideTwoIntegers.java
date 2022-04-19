1e4ac05e814e48166a5f5a06cc7f96e3da0ff6f2
leiqjl <leiqjl@gmail.com>
2022-04-19 17:50:43 +0800

    @add Divide Two Integers

--- src/com/leiqjl/DivideTwoIntegers.java
+++ src/com/leiqjl/DivideTwoIntegers.java
@@ -1,9 +1,28 @@
 package com.leiqjl;
 
+/**
+ * 29. Divide Two Integers - Medium
+ */
 public class DivideTwoIntegers {
 
     public int divide(int dividend, int divisor) {
-        
-        return 0;
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        int dividendAbs = Math.abs(dividend), divisorAbs = Math.abs(divisor);
+        int res = 0;
+        while (dividendAbs - divisorAbs >= 0) {
+            int n = 1, tmp = divisorAbs;
+            while (dividendAbs - (tmp << 1) >= 0) {
+                tmp <<= 1;
+                n <<= 1;
+            }
+            dividendAbs -= tmp;
+            res += n;
+        }
+        if ((dividend > 0) ^ (divisor > 0)) {
+            return -res;
+        }
+        return res;
     }
 }
