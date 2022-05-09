62a56f721ba26c3589c7cfd78ccac80d9369e32e
leiqjl <leiqjl@gmail.com>
2022-05-09 10:09:36 +0800

    @add Partition Equal Subset Sum

---
+++ src/com/leiqjl/PartitionEqualSubsetSum.java
@@ -0,0 +1,27 @@
+package com.leiqjl;
+
+/**
+ * 416. Partition Equal Subset Sum - Medium
+ */
+public class PartitionEqualSubsetSum {
+
+    //1 <= nums.length <= 200
+    //1 <= nums[i] <= 100
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 == 1) {
+            return false;
+        }
+        int target = sum / 2;
+        int[] dp = new int[target + 1];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = target; j >= nums[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
+            }
+        }
+        return dp[target] == target;
+    }
+}
