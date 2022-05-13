e0751529db41987a32bc6f76a442acc7b728452f
leiqjl <leiqjl@gmail.com>
2022-05-13 17:51:54 +0800

    @update Combination Sum III

--- src/com/leiqjl/CombinationSumIII.java
+++ src/com/leiqjl/CombinationSumIII.java
@@ -39,7 +39,6 @@ public class CombinationSumIII {
         CombinationSumIII c = new CombinationSumIII();
         //Input: k = 3, n = 7
         //Output: [[1,2,4]]
-        System.out.println(c.combinationSum3(3, 7));
         assert "[[1, 2, 4]]".equals(c.combinationSum3(3, 7).toString());
         //Input: k = 3, n = 9
         //Output: [[1,2,6],[1,3,5],[2,3,4]]
