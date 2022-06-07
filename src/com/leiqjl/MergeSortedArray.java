db4e17b9a30402f0cb8dba78d3d399292e2f8f14
leiqjl <leiqjl@gmail.com>
2022-06-07 17:27:02 +0800

    @update Merge Sorted Array

--- src/com/leiqjl/MergeSortedArray.java
+++ src/com/leiqjl/MergeSortedArray.java
@@ -1,28 +1,31 @@
 package com.leiqjl;
 
 /**
- * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
- *
- * Note:
- * The number of elements initialized in nums1 and nums2 are m and n respectively.
- * You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
- *
+ * 88. Merge Sorted Array - Easy
+ * <p>
+ * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
+ * and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+ * <p>
+ * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+ * <p>
+ * The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+ * To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged,
+ * and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
  */
 public class MergeSortedArray {
-
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int k = m+n-1;
+        int idx = m + n - 1;
         m--;
         n--;
         while (m >= 0 && n >= 0) {
             if (nums1[m] >= nums2[n]) {
-                nums1[k--] = nums1[m--];
+                nums1[idx--] = nums1[m--];
             } else {
-                nums1[k--] = nums2[n--];
+                nums1[idx--] = nums2[n--];
             }
         }
         while (n >= 0) {
-            nums1[k--] = nums2[n--];
+            nums1[idx--] = nums2[n--];
         }
     }
 }
