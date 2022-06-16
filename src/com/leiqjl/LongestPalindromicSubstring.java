54291d333237ccecd1edb70765d28f853da4d81d
leiqjl <leiqjl@gmail.com>
2022-06-17 00:50:57 +0800

    @update Longest Palindromic Substring

--- src/com/leiqjl/LongestPalindromicSubstring.java
+++ src/com/leiqjl/LongestPalindromicSubstring.java
@@ -1,39 +1,36 @@
 package com.leiqjl;
 
 /**
+ * 5. Longest Palindromic Substring - Medium
+ * <p>
  * Given a string s, find the longest palindromic substring in s.
- * You may assume that the maximum length of s is 1000.
  */
 public class LongestPalindromicSubstring {
-    private int max = 0;
-    private String result = null;
 
     public String longestPalindrome(String s) {
         if (s == null || s.length() <= 1) {
             return s;
         }
-        max = 1;
-        result = s.substring(0, 1);
+        int lo = 0, maxLen = 0;
         for (int i = 0; i < s.length(); i++) {
-            checkPalindrome(s, i, i);
-            checkPalindrome(s, i, i + 1);
-        }
-        return result;
-    }
-
-    private void checkPalindrome(String s, int i, int j) {
-        while (i >= 0 && j <= s.length() - 1) {
-            if (s.charAt(i) == s.charAt(j)) {
-                if ((j - i + 1) > max) {
-                    max = j - i + 1;
-                    result = s.substring(i, j + 1);
-                }
-                i--;
-                j++;
-            } else {
-                return;
+            int len1 = expand(s, i, i);
+            int len2 = expand(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > maxLen) {
+                maxLen = len;
+                lo = i - (len - 1) / 2;
             }
         }
+        return s.substring(lo, lo + maxLen);
+    }
+
+    private int expand(String s, int l, int r) {
+        int left = l, right = r;
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
     }
 
     //Manacher
@@ -49,11 +46,11 @@ public class LongestPalindromicSubstring {
             int mi = 2 * C - i;
             P[i] = (R > i) ? Math.min(R - i, P[mi]) : 0;
 
-            int a = i + P[i]+1, b=i - P[i]-1;
-            while (b>=0 && a<=len-1 && str.charAt(a) == str.charAt(b)) {
+            int a = i + P[i] + 1, b = i - P[i] - 1;
+            while (b >= 0 && a <= len - 1 && str.charAt(a) == str.charAt(b)) {
                 P[i]++;
-                a = i + P[i]+1;
-                b=i - P[i]-1;
+                a = i + P[i] + 1;
+                b = i - P[i] - 1;
             }
             if (i + P[i] > R) {
                 C = i;
@@ -69,7 +66,7 @@ public class LongestPalindromicSubstring {
             }
         }
         int index = (centerIndex - maxLen) / 2;
-        return s.substring(index, index+maxLen);
+        return s.substring(index, index + maxLen);
     }
 
     private String processStr(String s) {
