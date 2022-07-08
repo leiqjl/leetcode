7b93309f3b8c754cc3e8a9f3ad6c0dc376a1a118
leiqjl <leiqjl@gmail.com>
2022-07-08 10:58:08 +0800

    @update Paint House

--- src/com/leiqjl/PaintHouse.java
+++ src/com/leiqjl/PaintHouse.java
@@ -21,6 +21,16 @@ public class PaintHouse {
         return Math.min(dp[n][0], Math.min(dp[n][1], dp[n][2]));
     }
 
+    public int minCost1(int[][] costs) {
+        int n = costs.length;
+        for (int i = 1; i < n; i++) {
+            costs[i][0] += Math.min(costs[i - 1][1], costs[i - 1][2]);
+            costs[i][1] += Math.min(costs[i - 1][0], costs[i - 1][2]);
+            costs[i][2] += Math.min(costs[i - 1][0], costs[i - 1][1]);
+        }
+        return Math.min(costs[n - 1][0], Math.min(costs[n - 1][1], costs[n - 1][2]));
+    }
+
     public static void main(String[] args) {
         PaintHouse p = new PaintHouse();
         //Input: [[17,2,17],[16,16,5],[14,3,19]]
