7b594f5c00e8f41ecc14f5cfa06575b15cc3b49f
leiqjl <leiqjl@gmail.com>
2022-08-12 15:56:41 +0800

    @add Lowest Common Ancestor of a Binary Search Tree

---
+++ src/com/leiqjl/LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,16 @@
+package com.leiqjl;
+
+/**
+ * 235. Lowest Common Ancestor of a Binary Search Tree - Easy
+ */
+public class LowestCommonAncestorOfABinarySearchTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+}
