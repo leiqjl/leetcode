84db3180e0328fa6d6ab978fe32f75af26617c7c
leiqjl <leiqjl@gmail.com>
2023-02-01 23:32:07 +0800

    @add Greatest Common Divisor of Strings

---
+++ src/com/leiqjl/GreatestCommonDivisorOfStrings.java
@@ -0,0 +1,17 @@
+package com.leiqjl;
+
+/**
+ * 1071. Greatest Common Divisor of Strings - Easy
+ */
+public class GreatestCommonDivisorOfStrings {
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+        return str1.substring(0, gcd(str1.length(), str2.length()));
+    }
+
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+}
