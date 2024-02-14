613ba8bb930086b5f67279f202e353aa2bfaf3ac
leiqjl <leiqjl@gmail.com>
2024-02-15 00:01:24 +0800

    Add Rearrange Array Elements by Sign

---
+++ src/com/leiqjl/RearrangeArrayElementsBySign.java
@@ -0,0 +1,22 @@
+package com.leiqjl;
+
+/**
+ * 2149. Rearrange Array Elements by Sign - Medium
+ */
+public class RearrangeArrayElementsBySign {
+    public int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        int posIdx = 0, negIdx = 1;
+        for (int num : nums) {
+            if (num > 0) {
+                res[posIdx] = num;
+                posIdx += 2;
+            } else {
+                res[negIdx] = num;
+                negIdx += 2;
+            }
+        }
+        return res;
+    }
+}
