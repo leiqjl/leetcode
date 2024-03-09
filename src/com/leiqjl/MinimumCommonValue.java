0e86adba40efe0a1f83a531cde21e33d10e19339
leiqjl <leiqjl@gmail.com>
2024-03-09 22:50:59 +0800

    Add Minimum Common Value

---
+++ src/com/leiqjl/MinimumCommonValue.java
@@ -0,0 +1,20 @@
+package com.leiqjl;
+
+/**
+ * 2540. Minimum Common Value - Easy
+ */
+public class MinimumCommonValue {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                return nums1[i];
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return -1;
+    }
+}
