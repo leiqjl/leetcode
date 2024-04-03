9621d51a4732e061255736fff0d04368d06f7e65
leiqjl <leiqjl@gmail.com>
2024-04-04 00:58:51 +0800

    Add Isomorphic Strings

---
+++ src/com/leiqjl/IsomorphicStrings.java
@@ -0,0 +1,19 @@
+package com.leiqjl;
+
+/**
+ * 205. Isomorphic Strings - Easy
+ */
+public class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        int[] arr1 = new int[128];
+        int[] arr2 = new int[128];
+        for (int i = 0; i < s.length(); i++) {
+            if (arr1[s.charAt(i)] != arr2[t.charAt(i)]) {
+                return false;
+            }
+            arr1[s.charAt(i)] = i + 1;
+            arr2[t.charAt(i)] = i + 1;
+        }
+        return true;
+    }
+}
