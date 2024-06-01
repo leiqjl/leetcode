a562a8aecb2929eeab8fa8700ccfd0133bd83678
leiqjl <leiqjl@gmail.com>
2024-06-01 22:48:48 +0800

    Add Score of a String

---
+++ src/com/leiqjl/ScoreOfAString.java
@@ -0,0 +1,14 @@
+package com.leiqjl;
+
+/**
+ * 3110. Score of a String - Easy
+ */
+public class ScoreOfAString {
+    public int scoreOfString(String s) {
+        int score = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            score += Math.abs(s.charAt(i) - s.charAt(i + 1));
+        }
+        return score;
+    }
+}
