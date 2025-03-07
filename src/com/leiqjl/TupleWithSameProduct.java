59bffee666e774b2e357e6aad8ee51dfab47b7e5
leiqjl <leiqjl@gmail.com>
2025-03-07 16:19:06 +0800

    Update Tuple with Same Product

--- src/com/leiqjl/TupleWithSameProduct.java
+++ src/com/leiqjl/TupleWithSameProduct.java
@@ -31,10 +31,16 @@ public class TupleWithSameProduct {
         TupleWithSameProduct t = new TupleWithSameProduct();
         //Input: nums = [2,3,4,6]
         //Output: 8
+        //(2,6,3,4) , (2,6,4,3) , (6,2,3,4) , (6,2,4,3)
+        //(3,4,2,6) , (4,3,2,6) , (3,4,6,2) , (4,3,6,2)
         int[] nums = {2, 3, 4, 6};
         assert t.tupleSameProduct(nums) == 8;
         //Input: nums = [1,2,4,5,10]
         //Output: 16
+        //(1,10,2,5) , (1,10,5,2) , (10,1,2,5) , (10,1,5,2)
+        //(2,5,1,10) , (2,5,10,1) , (5,2,1,10) , (5,2,10,1)
+        //(2,10,4,5) , (2,10,5,4) , (10,2,4,5) , (10,2,5,4)
+        //(4,5,2,10) , (4,5,10,2) , (5,4,2,10) , (5,4,10,2)
         int[] nums1 = {1, 2, 4, 5, 10};
         assert t.tupleSameProduct(nums1) == 16;
     }
