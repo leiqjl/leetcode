44af9bc8c52f3e299aa4fec3e1e7f0ec2b7ecd12
leiqjl <leiqjl@gmail.com>
2025-08-20 22:07:26 +0800

    Update Maximal Square

--- src/com/leiqjl/MaximalSquare.java
+++ src/com/leiqjl/MaximalSquare.java
@@ -4,21 +4,43 @@ package com.leiqjl;
  * 221. Maximal Square - Medium
  */
 public class MaximalSquare {
+
     public int maximalSquare(char[][] matrix) {
         int max = 0;
         int m = matrix.length;
         int n = matrix[0].length;
-        int[][] dp = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 || j == 0 || matrix[i][j] == '0') {
-                    dp[i][j] = matrix[i][j] - '0';
+        int[] dp = new int[n + 1];
+        int prev = 0;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    int temp = dp[j];
+                    dp[j] = Math.min(Math.min(temp, dp[j - 1]), prev) + 1;
+                    prev = temp;
                 } else {
+                    dp[j] = 0;
+                }
+                max = Math.max(max, dp[j]);
+            }
+        }
+        return max * max;
+    }
+
+    public int maximalSquare1(char[][] matrix) {
+        int max = 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
                     dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                } else {
+                    dp[i][j] = 0;
                 }
                 max = Math.max(max, dp[i][j]);
             }
         }
-        return max;
+        return max * max;
     }
 }
