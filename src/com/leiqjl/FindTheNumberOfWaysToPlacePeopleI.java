f1249e0a9a714241b6e4ce448f0b009fd34bee59
leiqjl <leiqjl@gmail.com>
2025-10-16 15:11:10 +0800

    Add Find the Number of Ways to Place People I

---
+++ src/com/leiqjl/FindTheNumberOfWaysToPlacePeopleI.java
@@ -0,0 +1,43 @@
+package com.leiqjl;
+
+import java.util.Arrays;
+
+/**
+ * 3025. Find the Number of Ways to Place People I - Medium
+ */
+public class FindTheNumberOfWaysToPlacePeopleI {
+    public int numberOfPairs(int[][] points) {
+        int n = points.length;
+        //left, top    right, bottom
+        Arrays.sort(points, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            int x1 = points[i][0], y1 = points[i][1];
+            int max = Integer.MIN_VALUE;
+            for (int j = i + 1; j < n; j++) {
+                int x2 = points[j][0], y2 = points[j][1];
+                if (y2 > y1) {
+                    continue;
+                }
+                if (y2 > max) {
+                    count++;
+                    max = y2;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        FindTheNumberOfWaysToPlacePeopleI f = new FindTheNumberOfWaysToPlacePeopleI();
+        //Input: points = [[1,1],[2,2],[3,3]]
+        //Output: 0
+        assert f.numberOfPairs(new int[][]{{1, 1}, {2, 2}, {3, 3}}) == 0;
+        //Input: points = [[6,2],[4,4],[2,6]]
+        //Output: 2
+        assert f.numberOfPairs(new int[][]{{6, 2}, {4, 4}, {2, 6}}) == 2;
+        //Input: points = [[3,1],[1,3],[1,1]]
+        //Output: 2
+        assert f.numberOfPairs(new int[][]{{3, 1}, {1, 3}, {1, 1}}) == 2;
+    }
+}
