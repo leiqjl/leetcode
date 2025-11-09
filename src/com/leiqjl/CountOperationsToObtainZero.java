da4da86264f05d5008df01dc1f9f8aeb2520494b
leiqjl <leiqjl@gmail.com>
2025-11-09 16:16:19 +0800

    Add Count Operations to Obtain Zero

---
+++ src/com/leiqjl/CountOperationsToObtainZero.java
@@ -0,0 +1,19 @@
+package com.leiqjl;
+
+/**
+ * 2169. Count Operations to Obtain Zero - Easy
+ */
+public class CountOperationsToObtainZero {
+    public int countOperations(int num1, int num2) {
+        int count = 0;
+        while (num1 != 0 && num2 != 0) {
+            if (num1 >= num2) {
+                num1 -= num2;
+            } else {
+                num2 -= num1;
+            }
+            count++;
+        }
+        return count;
+    }
+}
