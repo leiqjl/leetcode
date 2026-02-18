724518f99ccd8d8d33dde3676f980fdb1aa959f4
leiqjl <leiqjl@gmail.com>
2026-02-18 23:10:28 +0800

    Add Binary Number with Alternating Bits

---
+++ src/com/leiqjl/BinaryNumberWithAlternatingBits.java
@@ -0,0 +1,20 @@
+package com.leiqjl;
+
+/**
+ * 693. Binary Number with Alternating Bits - Easy
+ */
+public class BinaryNumberWithAlternatingBits {
+    public boolean hasAlternatingBits(int n) {
+        int prev = n & 1;
+        n = n >> 1;
+        while (n > 0) {
+            int cur = n & 1;
+            if (cur == prev) {
+                return false;
+            }
+            prev = cur;
+            n = n >> 1;
+        }
+        return true;
+    }
+}
