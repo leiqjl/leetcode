28b7db5b221afacd1d6895dd32efc9f2b83778e1
leiqjl <leiqjl@gmail.com>
2026-05-03 21:24:53 +0800

    Add Rotate String

---
+++ src/com/leiqjl/RotateString.java
@@ -0,0 +1,15 @@
+package com.leiqjl;
+
+/**
+ * 796. Rotate String - Easy
+ * Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+ *
+ * A shift on s consists of moving the leftmost character of s to the rightmost position.
+ *
+ * For example, if s = "abcde", then it will be "bcdea" after one shift.
+ */
+public class RotateString {
+    public boolean rotateString(String s, String goal) {
+        return s.length() == goal.length() && (s + s).contains(goal);
+    }
+}
